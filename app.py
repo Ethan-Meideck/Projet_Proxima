249632f0d53d476cc3d86bb49ad8d0ad3a8e262f
NoRushJustPanda <ethan@meideck.net>
2026-04-08 16:55:19 +0200

    refactor: Deletion of useless sections

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ class App(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setup_ui()
-        self.setup_iss_tracking_menu()
-        self.setup_style()
-        
+
     def setup_ui(self):
         """Setting up the user interface.
         """
@@ -32,46 +30,15 @@ class App(QMainWindow):
         self.button_apod = QPushButton("Image du jour")
 
         # Adding widgets to layouts
-        self.layout_tracking.addWidget(self.button_iss_tracking)
-        self.layout_tracking.addWidget(self.button_astronauts_tracking)
-
-        self.layout_apod.addWidget(self.button_apod)
+        self.layout_main.addWidget(self.button_iss_tracking)
+        self.layout_main.addWidget(self.button_apod)
 
         # Adding layouts to main window
         self.setCentralWidget(self.widget_main)
         self.widget_main.setLayout(self.layout_main)
 
-        self.layout_main.addLayout(self.layout_tracking)
-        self.layout_main.addLayout(self.layout_apod)
-
-        self.stack_menu = QStackedLayout()
-
-        self.stack_menu.addWidget(self.widget_main)
-
-    def setup_iss_tracking_menu(self):
-        self.world_map = r"sources\World_map.jpg"
-        self.widget_iss_tracking_menu = QWidget()
-        self.layout_iss_tracking_menu = QHBoxLayout()
-
-        self.label_world_map = QLabel(self)
-        self.pixmap_world_map = QPixmap(self.world_map)
-        self.label_world_map.setPixmap(self.pixmap_world_map)
-
-        self.widget_iss_tracking_menu.setLayout(self.layout_iss_tracking_menu)
-        self.layout_iss_tracking_menu.addWidget(self.label_world_map)
-
-        self.stack_menu.addWidget(self.widget_iss_tracking_menu)
-
     def setup_connection(self):
-        self.button_iss_tracking.clicked.connect(self.stack_menu.setCurrentIndex(1))
-    
-    def setup_style(self):
-        """Editing the style of each widgets
-        """
-        # Edit button size
-        self.button_iss_tracking.setFixedSize(330, 300)
-        self.button_astronauts_tracking.setFixedSize(330, 300)
-        self.button_apod.setFixedSize(700, 300)
+        pass
 
 app = QApplication([])
 
